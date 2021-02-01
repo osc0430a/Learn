a1bd284dd234f9265879cef8acf07d84dd2a2963
osc0430a <71375659+osc0430a@users.noreply.github.com>
2021-02-01 09:25:25 +0900

    Add files via upload

--- 형상/코딩테스트/코드/Java/D0129/Endian.java
+++ 형상/코딩테스트/코드/Java/D0129/Endian.java
@@ -7,17 +7,17 @@ public class Endian {
 		System.out.println("INPUT = " + input);
 		String binary = Integer.toBinaryString(input);
 		
-		while(binary.length() != 32) {
+		while(binary.length() < 32) {
 			binary = "0" + binary;
 		}
 		
-		System.out.print("BINARY = ");
 		String fir, sec, thi, fou;
 		fir = binary.substring(0, 8);
 		sec = binary.substring(8, 16);
 		thi = binary.substring(16, 24);
 		fou = binary.substring(24, 32);
-		
+	
+		System.out.print("BIG ENDIAN BINARY = ");
 		for(int i=0; i<binary.length(); i++) {
 			if(i%8 == 0)
 				System.out.print(" ");
@@ -26,6 +26,14 @@ public class Endian {
 		System.out.println();
 		
 		binary = fou + thi + sec + fir;
+		
+		System.out.print("LITTLE ENDIAN BINARY = ");
+		for(int i=0; i<binary.length(); i++) {
+			if(i%8 == 0)
+				System.out.print(" ");
+			System.out.print(binary.charAt(i));
+		}
+		System.out.println();
 		
 		int answer = Integer.parseInt(binary, 2);
 		System.out.println("ANSWER = " + answer);
