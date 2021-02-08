031ba187938f9f52435089fb77db36516b8d8d34
OWNER <OWNER@DESKTOP-EIEO8UA>
2021-02-08 15:24:03 +0900

    2021-02-08 Final

--- Example/src/CodingTest/D0208/Pilfer.java
+++ Example/src/CodingTest/D0208/Pilfer.java
@@ -2,9 +2,10 @@
 package CodingTest.D0208;
 
 public interface Pilfer {
-	final static int[] money1 = {1, 2, 3, 1, 5, 6};
+	final static int[] money1 = {5, 2, 3, 1, 1, 5, 6};
 	final static int[] money2 = {1, 2, 3, 1};
-	static int[] money = money1;
+	static int[] money = money2;
+	static int[] check = new int[money.length];
 	
 	private static int calMoney(int sum, int now, boolean steal) {
 		int stealNext = 0;
@@ -19,8 +20,13 @@ public interface Pilfer {
 			stealNext = calMoney(sum, now+1, true);
 			notStealNext = calMoney(sum, now+1, false);
 		}
-		if(stealNext > notStealNext)
+		if(stealNext > notStealNext) {
+			if(now +1 < check.length)
+				check[now+1] = 1;
 			return stealNext;
+		}
+		if(now +1 < check.length)
+			check[now+1] = 0;
 		return notStealNext;
 	}
 	
@@ -28,8 +34,10 @@ public interface Pilfer {
 		int stealFirst = calMoney(0, 0, true);
 		int notStealFirst = calMoney(0, 0, false);
 		
-		if(stealFirst > notStealFirst)
+		if(stealFirst > notStealFirst) {
+			check[0] = 1;
 			return stealFirst;
+		}
 		return notStealFirst;
 	}
 
@@ -41,6 +49,14 @@ public interface Pilfer {
 		System.out.println();
 		
 		int answer = solution();
+		
+		System.out.print("STEAL = [ ");
+		for(int i=0; i<check.length; i++) {
+			if(check[i] == 1)
+				System.out.print(i + " ");
+		}
+		System.out.println("]");
+		
 		System.out.println("ANSWER = " + answer);
 	}	
 }
