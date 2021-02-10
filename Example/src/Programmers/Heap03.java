092d43a745493d34f7e3cb4ff822eb0e9d761b11
OWNER <OWNER@DESKTOP-EIEO8UA>
2021-02-10 15:31:00 +0900

    2021-02-10

--- Example/src/Programmers/Heap03.java
+++ Example/src/Programmers/Heap03.java
@@ -1,160 +1,134 @@
 package Programmers;
 
+import java.util.ArrayList;
+
 public class Heap03 {	
-	private Node top = null;
-	private Node bottom = null;
+	ArrayList<Integer> hp = new ArrayList<Integer>();
+	int max = 0;
 	
-	class Node{
-		private int v;
-		private Node next_t;
-		private Node next_b;
+	private void add(int v) {
+		hp.add(v);
+		if(v > max)
+			max = v;
+		int now = hp.size() - 1;
+		int parent = (now - 1) / 2;
 		
-		Node(int v){
-			this.v = v;
-		}
-	
-		private int getV() {
-			return this.v;
-		}
-		
-		private Node getNextTop() {
-			return next_t;
-		}
-		
-		private Node getNextBottom() {
-			return next_b;
-		}
-		
-		private void setNextTop(Node top) {
-			this.next_t = top;
-		}
-		
-		private void setNextBottom(Node bottom) {
-			this.next_b = bottom;
-		}
-	}
-	
-	private void push(int v) {
-		Node n = new Node(v);
-		
-		if(top == null) {
-			top = n;
-			bottom = n;
-			return;
-		}
-		
-		Node tmp = top;
-		
-		while (tmp != null) {
-			if(tmp.getV() <= v) {
-				top.setNextBottom(n);
-				n.setNextTop(top);
-				top = n;
-				return;
-			}
-			else if(tmp.getNextTop() == null) {
-				top.setNextTop(n);
-				n.setNextBottom(top);
-				bottom = n;
-				return;
+		while(parent >= 0) {
+			int p = hp.get(parent);
+			int s = hp.get(now);
+			
+			if(p > s) {
+				hp.set(parent, s);
+				hp.set(now, p);
+				now = parent;
+				parent = (now - 1) / 2;
 			}
 			else
-				tmp = tmp.getNextTop();
-		}		
+				break;
+		}
 	}
 	
-	private int popTop() {
-		if(top == null)
+	private int popMin() {
+		if(hp.size() == 0)
 			return 0;
-		int pop = top.getV();
-		if(top.getNextTop() != null) {
-			top = top.getNextTop();
-			top.setNextBottom(null);
-			if(top == null)
-				bottom = null;
-		}
-		else {
-			top = null;
-			bottom = null;
+		if(hp.size() == 1) {
+			int min = hp.get(0);
+			hp.remove(0);
+			return min;
 		}
 		
-		return pop;
-	}
-	
-	private int popBottom() {
-		if(bottom == null)
-			return 0;
-		int pop = bottom.getV();
-		if(bottom.getNextBottom() != null) {
-			bottom = bottom.getNextBottom();
-			bottom.setNextTop(null);
-			if(bottom == null)
-				top = null;
-		}
-		else {
-			top = null;
-			bottom = null;
+		int min = hp.get(0);
+		hp.set(0, hp.get(hp.size()-1));
+		hp.remove(hp.size()-1);
+		
+		int now = 0;
+		int leftSon = now * 2 + 1;
+		int rightSon = now * 2 + 2;
+		while(leftSon < hp.size()) {
+			int l = hp.get(leftSon);
+			int r = 999999999;
+			if(rightSon < hp.size())
+				r = hp.get(rightSon);
+			int p = hp.get(now);
+			if(l < r){
+				if(l < p) {
+					hp.set(now, l);
+					hp.set(leftSon, p);
+					now = leftSon;
+					leftSon = now * 2;
+					rightSon = now * 2 + 1;
+				}
+				else
+					break;
+			}
+			else {
+				if(r < p) {
+					hp.set(now, r);
+					hp.set(rightSon, p);
+					now = rightSon;
+					leftSon = now * 2;
+					rightSon = now * 2 + 1;
+				}
+				else
+					break;
+			}
 		}
 		
-		return pop;
+		return min;
 	}
 	
-	private Node getTop() {
-		return top;
-	}
-
-	private Node getBottom() {
-		return bottom;
-	}
-	
-	private int getTopV() {
-		return top.getV();
-	}
-	
-	private int getBottomV() {
-		return bottom.getV();
+	private int popMax() {
+		if(hp.size() == 0)
+			return 0;
+		if(hp.size() == 1) {
+			int tmpMax = hp.get(0);
+			hp.remove(0);
+			return tmpMax;
+		}
+		int cnt = 0;
+		int tmpMax = max;
+		max = 0;
+		int index = hp.indexOf(tmpMax);
+		hp.remove(index);
+		int i = 0;
+		
+		while(index - cnt >= 0) {
+			cnt += (int)Math.pow(2, i++);
+		}
+//		int depth = i;
+		int start = cnt - (int)Math.pow(2, i - 1);
+		int finish = 0;
+		
+		if(cnt > hp.size())
+			finish = hp.size();
+		else
+			finish = cnt;
+		
+		for(i=start; i<finish; i++) {
+			int tmp = hp.get(i);
+			if(tmp > max)
+				max = tmp;
+		}
+		
+		return tmpMax;
 	}
 	
 	public static void main(String[] args) {
-		String[] input1 = {"I 16", "I -5643", "D -1", "D 1", "D 1", "I 123", "D -1"};
-		String[] input2 = {"I -45", "I 653", "D 1", "I -642", "I 45", "I 97", "D 1", "D -1", "I 333"};
 		Heap03 h = new Heap03();
-		int answer[] = new int[2];
+		h.add(-45);
+		h.add(653);
+		h.popMax();
+		h.add(-642);
+		h.add(45);
+		h.add(97);
+		h.popMax();
+		h.popMin();
+		h.add(333);
 		
-		for(int i=0; i<input2.length; i++) {
-			String[] oper = input2[i].split(" ");
-			
-			if(oper[0].equals("I")) {
-				h.push(Integer.parseInt(oper[1]));
-			}			
-			else if(oper[1].charAt(0) == '-') {
-				h.popBottom();
-			}
-			else {
-				h.popTop();
-			}
-			
-			Node tmp = h.getTop();
-			System.out.println();
-			
-			System.out.println("TOP");
-			while(tmp != null) {
-				System.out.print(tmp.getV() + " ");
-				tmp = tmp.getNextTop();
-			}
-			System.out.println();
-			System.out.println("BOTTOM");
-			tmp = h.getBottom();
-			while(tmp != null) {
-				System.out.print(tmp.getV() + " ");
-				tmp = tmp.getNextBottom();
-			}
-			System.out.println();
-		}
-
-		answer[0] = h.getTopV();
-		answer[1] = h.getBottomV();
-		
+		int[] answer = new int[2];
+		answer[0] = h.popMax();
+		answer[1] = h.popMin();
+		System.out.println("ANSWER = [" + answer[0] + ", " + answer[1] + "]");
 	}
 
 }
